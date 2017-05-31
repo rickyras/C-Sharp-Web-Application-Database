391e5abf94b1ad12de7adcd56d44d181a56beef0
rickyras <ritchieperera@gmail.com>
2017-05-31 14:59:44 -0400

    3rd Commit

---
+++ DBConnectedModel/webform2 Assign.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="webform2 Assign.aspx.cs" Inherits="A3RitchiePerera.webform2_Assign" EnableSessionState="False" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+     
+    
+    
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Image ID="Image1" runat="server" Height="235px" ImageAlign="AbsMiddle" ImageUrl="~/Resources/foodtray.jpeg" Width="307px" />
+        <br />
+        <br />
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    
+     
+    
+    
+        <asp:Label ID="lblCategory" runat="server" Text="Categories" BackColor="#CC6600" EnableTheming="True" Font-Bold="True" Font-Italic="False" Font-Size="Larger" ForeColor="#FFCC66" Height="27px" style="margin-top: 0px" Width="116px"></asp:Label>
+
+        <asp:ListBox ID="lbCat" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" AppendDataBoundItems="True" BackColor="#FFFF66" Font-Italic="True" Font-Size="Medium" ForeColor="#CC6600" Height="71px" Width="153px"></asp:ListBox>
+        <br />
+        <br />
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblProduct" runat="server" Text="Products" BackColor="#FF9900" BorderStyle="Double" Font-Size="Larger" ForeColor="#FFFF66" Height="26px" Width="100px"></asp:Label>
+        <asp:DropDownList ID="ddlProducts" runat="server"
+             AutoPostBack="True" OnSelectedIndexChanged="ddlProducts_SelectedIndexChanged"
+             Enabled="False" AppendDataBoundItems="True" ViewStateMode="Enabled" BackColor="#FFFF66" Font-Italic="True" ForeColor="#993300">
+        
+       </asp:DropDownList>
+    
+        <br />
+        <br />
+        <br />
+        <br />
+    
+    </div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:GridView ID="grdOrder" runat="server" AutoGenerateColumns="False" 
+            EmptyDataText="There are no data records to display." BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2" Enabled="False" EnableViewState="False" OnSelectedIndexChanged="grdOrder_SelectedIndexChanged">
+            <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+            <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+            <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#FFF1D4" />
+            <SortedAscendingHeaderStyle BackColor="#B95C30" />
+            <SortedDescendingCellStyle BackColor="#F1E5CE" />
+            <SortedDescendingHeaderStyle BackColor="#93451F" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ DBConnectedModel/webform2 Assign.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace A3RitchiePerera
+{
+    public partial class webform2_Assign : System.Web.UI.Page
+    {
+
+        string cs = ConfigurationManager.ConnectionStrings["NorthwindConnectionString1"].ConnectionString;
+        
+        DataSet ds = new DataSet();
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+           
+            string query1 = "Select CategoryID, CategoryName from Categories;";
+
+           
+
+            try
+            {
+                if (!IsPostBack)
+                {
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query1, conn);
+                        conn.Open();
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        lbCat.DataSource = reader;
+                        lbCat.DataTextField = "CategoryName";
+                        lbCat.DataValueField = "CategoryID";
+                        lbCat.DataMember = "Order";
+                        lbCat.DataBind();
+                        ddlProducts.Enabled = false;
+                        ListItem liSelect = new ListItem("Select a Product", "-1");
+                        ddlProducts.Items.Insert(0, liSelect);
+                        ds.Clear();
+
+                    }
+
+                    
+
+                }
+            }
+            catch (HttpParseException ex)
+            {
+
+            }
+            catch (HttpCompileException ex)
+            {
+
+            }
+            catch (InvalidOperationException ex)
+            {
+
+            }
+            finally
+            {
+
+                ds.Clear();
+
+            }
+
+
+
+        }//enable postback on the dropdownlist
+
+
+protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string index = lbCat.SelectedValue;
+            ddlProducts.Items.Clear();
+            ds.Clear();
+            grdOrder.AutoGenerateColumns = false;
+            ddlProducts.Enabled = true;
+           
+            //something to do with index value and value of productID!!!!!!!
+            string query = "SELECT ProductID, ProductName FROM Products WHERE (Products.CategoryID ='" + index + "');";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    conn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    ddlProducts.DataSource = reader;
+
+
+                    ddlProducts.DataTextField = "ProductName";
+                    ddlProducts.DataValueField = "ProductID";
+                    ddlProducts.DataMember = "Order";
+
+                    ddlProducts.DataBind();
+
+                }
+            }
+            catch (SqlException ex)
+            {
+
+            }
+            finally
+            {
+                grdOrder.AutoGenerateColumns = false;
+            }
+   
+        }
+
+        protected void ddlProducts_SelectedIndexChanged(object sender, EventArgs e)
+
+        {
+
+            string index = lbCat.SelectedItem.Value;
+
+            string index2 = ddlProducts.SelectedValue;
+            //must set Auto Generate Coluumns to true on gridview
+            //make sure enable view state of false so that it does not inherit previous displaying
+
+            string query = "SELECT Orders.OrderID, Orders.OrderDate, Orders.ShippedDate, Orders.ShipName, Orders.ShipAddress, Orders.ShipCity, Orders.ShipCountry AS ShipCountry FROM Orders INNER JOIN [Order Details] ON Orders.OrderID = [Order Details].OrderID INNER JOIN Products ON [Order Details].ProductID = Products.ProductID INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID WHERE (Products.ProductID ='" + index2 + "');";
+
+
+            string curItem = lbCat.SelectedItem.ToString();
+
+
+            try
+            {
+                if (!ddlProducts.SelectedValue.Equals(-1))
+                {
+                    grdOrder.AutoGenerateColumns = true;
+                    SqlConnection conn = new SqlConnection(cs);
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    adapter.Fill(ds);
+                    ds.Tables[0].TableName = "Orders.OrderID";
+
+
+
+                    grdOrder.DataSource = ds.Tables["Orders.OrderID"];
+                    grdOrder.DataBind();
+
+
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                grdOrder.AutoGenerateColumns = true;               
+
+            }
+
+        }
+
+        protected void grdOrder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
